921e15e7c710b26843876595eafdea27d4c6f57c
James-L-Jr <james.f.logan2@gmail.com>
2022-01-20 22:52:35 -0600

    Adding ProjectItem

--- React/capstone/src/components/Project/ProjectItem.js
+++ React/capstone/src/components/Project/ProjectItem.js
@@ -2,14 +2,7 @@ import React, { Component } from "react";
 
 class ProjectItem extends Component {
   render() {
-    return (
-      <div>
-        <h1>Project Item</h1>;
-        <ProjectItem />
-        <ProjectItem />
-        <ProjectItem />
-      </div>
-    );
+    return <h1>Project Item</h1>;
   }
 }
 
