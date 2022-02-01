53dbbacef578457b0a43ce15b6f2b0dd02c5361c
James-L-Jr <james.f.logan2@gmail.com>
2022-01-31 19:52:41 -0600

    Token generation

--- Capstone/src/main/java/com/hcl/udemy/repositories/UserRepository.java
+++ Capstone/src/main/java/com/hcl/udemy/repositories/UserRepository.java
@@ -6,4 +6,6 @@ import com.hcl.udemy.domain.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+    User getById(Long id);
 }
